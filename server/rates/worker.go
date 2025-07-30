0f0a9e2e4d8d5da43ba044899283aa40428fe0aa
artem98 <temiko_oreshkin@bk.ru>
2025-07-31 01:42:16 +0300

    REF: refactor worker

--- server/rates/worker.go
+++ server/rates/worker.go
@@ -42,15 +42,29 @@ func start() {
 
 const useRealExternalApi = false
 
+func fetchRate(currency1, currency2 string) (float64, error) {
+	fmt.Println("Fetching rate for", currency1, "/", currency2)
+	var err error
+	var rate float64
+	if useRealExternalApi {
+		rate, err = fetchRateReal(currency1, currency2)
+	} else {
+		rate, err = fetchRateFake(currency1, currency2)
+	}
+
+	if err != nil {
+		fmt.Println("Failed to fetch rate:", err.Error())
+	}
+
+	return rate, err
+}
+
 func processJob(job Job) error {
 
 	var rate float64
 	var err error
-	if useRealExternalApi {
-		rate, err = fetchRateReal(job)
-	} else {
-		rate, err = fetchRateFake(job)
-	}
+
+	rate, err = fetchRate(job.Currency1, job.Currency2)
 
 	if err != nil {
 		markRequestAsFailed(job.reqId)
@@ -69,8 +83,8 @@ func processJob(job Job) error {
 var fakeRates = [...]float64{0.04, 0.89, 1.35, 33, 18.1, 9, 0.81, 0.33, 12.34, 2.93, 2.02, 1.09, 3.65, 0.11}
 var it = 0
 
-func fetchRateFake(job Job) (float64, error) {
-	time.Sleep(3 * time.Second)
+func fetchRateFake(currency1, currency2 string) (float64, error) {
+	time.Sleep(0 * time.Second)
 	it++
 	it = it % len(fakeRates)
 	return fakeRates[it], nil
@@ -83,9 +97,9 @@ type externalRateResponse struct {
 	Base  string             `json:"base"`
 }
 
-func fetchRateReal(job Job) (float64, error) {
+func fetchRateReal(currency1, currency2 string) (float64, error) {
 	url := fmt.Sprintf("https://api.exchangeratesapi.io/v1/latest?base=%s&symbols=%s&access_key=%s",
-		strings.ToUpper(job.Currency1), strings.ToUpper(job.Currency2), apiKey)
+		strings.ToUpper(currency1), strings.ToUpper(currency2), apiKey)
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -102,9 +116,9 @@ func fetchRateReal(job Job) (float64, error) {
 		return 0, err
 	}
 
-	rate, ok := parsed.Rates[job.Currency2]
+	rate, ok := parsed.Rates[currency2]
 	if !ok {
-		return 0, fmt.Errorf("rate for %s not found", job.Currency2)
+		return 0, fmt.Errorf("rate for %s not found", currency2)
 	}
 
 	return rate, nil
